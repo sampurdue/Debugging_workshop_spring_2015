cc46dde91f56e51d77bb15df30afafaab0fc5363
sampurdue <sampurdue@users.noreply.github.com>
2015-02-25 19:59:26 -0500

    Update malloc_mismaage.c

--- valgrind-examples-master/malloc_mismaage.c
+++ valgrind-examples-master/malloc_mismaage.c
@@ -6,14 +6,14 @@ int main()
   char *p;
 
   // Allocation #1 of 19 bytes
-  p = (char *) malloc(19);
+  p = (char *) malloc(19*sizeof(int));
 
   // Allocation #2 of 12 bytes
-  p = (char *) malloc(12);
+  p = (char *) malloc(12*sizeof(int));
   free(p);
 
   // Allocation #3 of 16 bytes
-  p = (char *) malloc(16);
+  p = (char *) malloc(16*sizeof(int));
 
   return 0;
 }
